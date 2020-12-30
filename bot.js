4c12a4c996664d31df1817e200ab8871ca9921a8
Your Name <you@example.com>
2020-12-30 17:49:07 +0530

    added gifs

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 console.log("Welcome to the singularity!!!😎 ")
 
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
+//const $ = require('jquery');
 const client = new Discord.Client();
 const env = require('./config/environment.js');
 
@@ -23,7 +25,7 @@ const replies = ['Namastey!!!! 🙏 ',
                  'kya haal hai!!!? Here have some milk 🥛',
                  'hey!! Beers on me 🍻',
                  "Finally the moon's out i guess"];
-client.on('message',function(msg){
+client.on('message',async function(msg){
 
     console.log(msg.content);
     //for replies in specific channels
@@ -35,15 +37,50 @@ client.on('message',function(msg){
 
             //if we wish to tag the person and reply
             // msg.reply('Namastey!!');
-
             //if we wish to leave user untagged    
             msg.channel.send(replies[index]);
         }
         else if(msg.content === '!gif'){
             console.log('!gif entered');
-        }
-
-    //}
+           /* $.ajax({
+                url: `https://api.tenor.com/v1/search?q=space&key=${env.gif_api_key}&contentfilter=off&limit=1`,
+                method: 'GET',
+                success: (data)=>{
+                    msg.channel.send(data.results.url);
+                    console.log(data.results.url);
+                }
+            })
+            console.log("is this running");
+            function loadXMLDoc() {
+                var xmlhttp = new XMLHttpRequest();
+            
+                xmlhttp.onreadystatechange = function() {
+                    console.log(xmlhttp.response);
+                    if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+                       if (xmlhttp.status == 200) {
+                           document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+                       }
+                       else if (xmlhttp.status == 400) {
+                          alert('There was an error 400');
+                       }
+                       else {
+                           alert('something else other than 200 was returned');
+                       }
+                    }
+                };
+            
+                xmlhttp.open("GET", `https://api.tenor.com/v1/search?q=space&key=${env.gif_api_key}&contentfilter=off&limit=1`, true);
+                xmlhttp.send();
+               console.log(xmlhttp.responseText);
+               msg.channel.send(data)
+            }
+        }*/
+        let url = `https://api.tenor.com/v1/search?q=space&key=${env.gif_api_key}&limit=1`;
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        msg.channel.send(json.results[0].url);
+    }
     
    //msg.reply('Namastey!!!! 🙏 ');
 
--- config/environment.js
+++ config/environment.js
@@ -1,8 +1,9 @@
 
 
-const development ={
+const development = {
     token :'NzkwOTE2NjcwMDcxNTA0OTE2.X-HkjA.HF_BoP41BNzUVueVQGJZ0zgJKNk',
-    client_id:'790916670071504916'
+    client_id:'790916670071504916',
+    gif_api_key: '9NUZAOBMGB65'
 }
 
 module.exports = development;
