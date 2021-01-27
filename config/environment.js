346ca2053480380020d7d5880db9c7c45ea8093e
Your Name <you@example.com>
2021-01-27 14:41:49 +0530

    Mistakes fixed

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 
 
 const development = {
-    token :'NzkwOTE2NjcwMDcxNTA0OTE2.X-HkjA.HF_BoP41BNzUVueVQGJZ0zgJKNk',
+    token :'NzkwOTE2NjcwMDcxNTA0OTE2.X-HkjA.f26VOThxIt6zrMe5VSQmcqJQwKk',
     client_id:'790916670071504916',
     gif_api_key: '9NUZAOBMGB65',
     nasa_key: 'r5uDKOy5J2Fjc3dyeMHmGNztqAkw2CYmTi6j8nCr'
